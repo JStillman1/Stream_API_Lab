b5e790a1c6b57135353d1f117be843286d5a8093
James Stillman <jamesstillman99@gmail.com>
2022-05-03 20:25:29 +0100

    End of exercises

--- start_code_lab_stream_api/src/main/java/org/example/streamapi/Extension.java
+++ start_code_lab_stream_api/src/main/java/org/example/streamapi/Extension.java
@@ -6,6 +6,7 @@ import org.example.streamapi.model.User;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Predicate;
 import java.util.stream.IntStream;
